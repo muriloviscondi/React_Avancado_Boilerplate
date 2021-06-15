532fc9288b74abf4f1478b1cfa95af6104f15121
Murilo Viscondi <50927186+muriloviscondi@users.noreply.github.com>
2021-06-15 06:58:33 -0300

    Criando a página Main

---
+++ src/components/Main/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 3rem;
+  text-align: center;
+  color: #fff;
+  background-color: #06092b;
+`
+export const Logo = styled.img`
+  width: 25rem;
+  margin-bottom: 2rem;
+`
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+`
+
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+`
+
+export const Illustration = styled.img`
+  width: min(30rem, 100%);
+  margin-top: 3rem;
+`
